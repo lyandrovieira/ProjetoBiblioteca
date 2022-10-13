42615b947fc958f1dfb10621dcc11c8e62a31291
lyandrovieira <lyandro.melo@gmail.com>
2022-10-12 21:55:51 -0300

    Mudança da classe da Tela Inicial/Criação tela de login.

--- Biblioteca/src/formularios/Principal.java
+++ Biblioteca/src/formularios/Principal.java
@@ -1,10 +1,11 @@
 package formularios;
 
+import javax.swing.JOptionPane;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-
 /**
  *
  * @author lyand
@@ -16,7 +17,6 @@ public class Principal extends javax.swing.JFrame {
      */
     public Principal() {
         initComponents();
-        this.setExtendedState(MAXIMIZED_BOTH);
     }
 
     /**
@@ -28,246 +28,146 @@ public class Principal extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        desktoppanel = new javax.swing.JDesktopPane();
-        jMenuBar1 = new javax.swing.JMenuBar();
-        menuAcervo = new javax.swing.JMenu();
-        subMenuCadExemplar = new javax.swing.JMenuItem();
-        subMenuConsultAcervo = new javax.swing.JMenuItem();
-        menuUsuarios = new javax.swing.JMenu();
-        subMenuCadUser = new javax.swing.JMenuItem();
-        subMenuConsultarUser = new javax.swing.JMenuItem();
-        subMenuAdmin = new javax.swing.JMenuItem();
-        menuEmpDev = new javax.swing.JMenu();
-        subMenuEmp = new javax.swing.JMenuItem();
-        subMenuDev = new javax.swing.JMenuItem();
-        subMenuConsultExemp = new javax.swing.JMenuItem();
-        jMenuItem1 = new javax.swing.JMenuItem();
-        menuSobre = new javax.swing.JMenu();
-        subMenuSobre = new javax.swing.JMenuItem();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        usuarioLogin = new javax.swing.JTextField();
+        senhaLogin = new javax.swing.JPasswordField();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBounds(new java.awt.Rectangle(0, 0, 0, 0));
         setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
         setSize(new java.awt.Dimension(790, 469));
 
-        javax.swing.GroupLayout desktoppanelLayout = new javax.swing.GroupLayout(desktoppanel);
-        desktoppanel.setLayout(desktoppanelLayout);
-        desktoppanelLayout.setHorizontalGroup(
-            desktoppanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 766, Short.MAX_VALUE)
-        );
-        desktoppanelLayout.setVerticalGroup(
-            desktoppanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 454, Short.MAX_VALUE)
-        );
+        jLabel1.setFont(new java.awt.Font("Arial", 0, 24)); // NOI18N
+        jLabel1.setText("Login");
 
-        menuAcervo.setText("Acervo");
+        jLabel2.setFont(new java.awt.Font("Arial", 0, 18)); // NOI18N
+        jLabel2.setText("Usuário");
 
-        subMenuCadExemplar.setText("Cadastrar Exemplar");
-        subMenuCadExemplar.addActionListener(new java.awt.event.ActionListener() {
+        jLabel3.setFont(new java.awt.Font("Arial", 0, 18)); // NOI18N
+        jLabel3.setText("Senha");
+
+        usuarioLogin.setFont(new java.awt.Font("Arial", 0, 18)); // NOI18N
+
+        senhaLogin.setFont(new java.awt.Font("Arial", 0, 18)); // NOI18N
+        senhaLogin.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                subMenuCadExemplarActionPerformed(evt);
-            }
-        });
-        menuAcervo.add(subMenuCadExemplar);
-
-        subMenuConsultAcervo.setText("Consultar Acervo");
-        subMenuConsultAcervo.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                subMenuConsultAcervoActionPerformed(evt);
-            }
-        });
-        menuAcervo.add(subMenuConsultAcervo);
-
-        jMenuBar1.add(menuAcervo);
-
-        menuUsuarios.setText("Usuários");
-
-        subMenuCadUser.setText("Cadastrar Usuários");
-        subMenuCadUser.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                subMenuCadUserActionPerformed(evt);
-            }
-        });
-        menuUsuarios.add(subMenuCadUser);
-
-        subMenuConsultarUser.setText("Usuários Cadastrados");
-        subMenuConsultarUser.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                subMenuConsultarUserActionPerformed(evt);
-            }
-        });
-        menuUsuarios.add(subMenuConsultarUser);
-
-        subMenuAdmin.setText("Administradores");
-        subMenuAdmin.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                subMenuAdminActionPerformed(evt);
-            }
-        });
-        menuUsuarios.add(subMenuAdmin);
-
-        jMenuBar1.add(menuUsuarios);
-
-        menuEmpDev.setText("Empréstimo/Devolução");
-
-        subMenuEmp.setText("Empréstimo");
-        subMenuEmp.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                subMenuEmpActionPerformed(evt);
-            }
-        });
-        menuEmpDev.add(subMenuEmp);
-
-        subMenuDev.setText("Devolução");
-        subMenuDev.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                subMenuDevActionPerformed(evt);
-            }
-        });
-        menuEmpDev.add(subMenuDev);
-
-        subMenuConsultExemp.setText("Consultar Empréstimos");
-        subMenuConsultExemp.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                subMenuConsultExempActionPerformed(evt);
-            }
-        });
-        menuEmpDev.add(subMenuConsultExemp);
-
-        jMenuItem1.setText("Consultar Devolução");
-        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem1ActionPerformed(evt);
-            }
-        });
-        menuEmpDev.add(jMenuItem1);
-
-        jMenuBar1.add(menuEmpDev);
-
-        menuSobre.setText("Sobre");
-        menuSobre.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                menuSobreActionPerformed(evt);
+                senhaLoginActionPerformed(evt);
             }
         });
 
-        subMenuSobre.setText("Sobre");
-        subMenuSobre.addActionListener(new java.awt.event.ActionListener() {
+        jButton1.setFont(new java.awt.Font("Arial", 0, 12)); // NOI18N
+        jButton1.setText("Ok");
+        jButton1.setPreferredSize(new java.awt.Dimension(84, 24));
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                subMenuSobreActionPerformed(evt);
+                jButton1ActionPerformed(evt);
             }
         });
-        menuSobre.add(subMenuSobre);
 
-        jMenuBar1.add(menuSobre);
+        jButton2.setFont(new java.awt.Font("Arial", 0, 12)); // NOI18N
+        jButton2.setText("Cancelar");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
-        setJMenuBar(jMenuBar1);
+        jLabel4.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
+        jLabel4.setText("Sistema de Gerenciamento de Biblioteca");
+
+        jLabel5.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
+        jLabel5.setText(" Colégio Estadual Adelino Antônio Gomide ");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(desktoppanel)
-                .addContainerGap())
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(36, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jLabel5, javax.swing.GroupLayout.Alignment.TRAILING)
+                        .addGroup(layout.createSequentialGroup()
+                            .addGap(10, 10, 10)
+                            .addComponent(jLabel4)
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 9, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(80, 80, 80)
+                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(62, 62, 62)
+                        .addComponent(jButton2))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(16, 16, 16)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jLabel3)
+                            .addComponent(jLabel2))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(usuarioLogin)
+                            .addComponent(senhaLogin, javax.swing.GroupLayout.PREFERRED_SIZE, 259, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGap(160, 160, 160)
+                        .addComponent(jLabel1)
+                        .addGap(157, 157, 157)))
+                .addGap(34, 34, 34))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(desktoppanel)
-                .addContainerGap())
+                .addGap(21, 21, 21)
+                .addComponent(jLabel4)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel5)
+                .addGap(28, 28, 28)
+                .addComponent(jLabel1)
+                .addGap(34, 34, 34)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(usuarioLogin, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(senhaLogin, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton2))
+                .addContainerGap(28, Short.MAX_VALUE))
         );
 
         pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    //Chama a tela de cadastro de exemplar.
-    private void subMenuCadExemplarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_subMenuCadExemplarActionPerformed
-        CadastroExemplar cadastroexemplar = new CadastroExemplar();
-        desktoppanel.add(cadastroexemplar);
-        cadastroexemplar.setPosicao();
-        cadastroexemplar.setVisible(true);
-    }//GEN-LAST:event_subMenuCadExemplarActionPerformed
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        if ((usuarioLogin.getText().equals("lyandro")) && (senhaLogin.getText().equals("1234"))) {
+            JOptionPane.showMessageDialog(null, "Acesso Garantido!");
+            TelaInicial inicial = new TelaInicial();
+            inicial.setVisible(true);
+        } else {
+            JOptionPane.showMessageDialog(null, "Acesso negado! Senha ou usuário incorreto!");
+        }
+    }//GEN-LAST:event_jButton1ActionPerformed
 
-    //Chama a tela de consulta de acervo.
-    private void subMenuConsultAcervoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_subMenuConsultAcervoActionPerformed
-        ConsultarAcervo consultaacervo = new ConsultarAcervo();
-        desktoppanel.add(consultaacervo);
-        consultaacervo.setPosicao();
-        consultaacervo.setVisible(true);
-    }//GEN-LAST:event_subMenuConsultAcervoActionPerformed
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        Principal.this.dispose();
+    }//GEN-LAST:event_jButton2ActionPerformed
 
-    //Chama a tela de cadastro de usuário.
-    private void subMenuCadUserActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_subMenuCadUserActionPerformed
-        CadastrarUsuarios cadusers = new CadastrarUsuarios();
-        desktoppanel.add(cadusers);
-        cadusers.setPosicao();
-        cadusers.setVisible(true);
-    }//GEN-LAST:event_subMenuCadUserActionPerformed
-
-    //Chama a tela de consulta de usuário.
-    private void subMenuConsultarUserActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_subMenuConsultarUserActionPerformed
-        UsuariosCadastrados userscad = new UsuariosCadastrados();
-        desktoppanel.add(userscad);
-        userscad.setPosicao();
-        userscad.setVisible(true);
-    }//GEN-LAST:event_subMenuConsultarUserActionPerformed
-
-    ////Chama a tela de consulta de administradores.
-    private void subMenuAdminActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_subMenuAdminActionPerformed
-        Administradores adms = new Administradores();
-        desktoppanel.add(adms);
-        adms.setPosicao();
-        adms.setVisible(true);
-    }//GEN-LAST:event_subMenuAdminActionPerformed
-
-    ////Chama a tela de empréstimo.
-    private void subMenuEmpActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_subMenuEmpActionPerformed
-        Emprestimo emp = new Emprestimo();
-        desktoppanel.add(emp);
-        emp.setPosicao();
-        emp.setVisible(true);
-    }//GEN-LAST:event_subMenuEmpActionPerformed
-
-    //Chama a tela de devolução.
-    private void subMenuDevActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_subMenuDevActionPerformed
-        Devolucao dev = new Devolucao();
-        desktoppanel.add(dev);
-        dev.setPosicao();
-        dev.setVisible(true);
-    }//GEN-LAST:event_subMenuDevActionPerformed
-
-    //Chama a tela de consulta de empréstimo.
-    private void subMenuConsultExempActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_subMenuConsultExempActionPerformed
-        ConsultarEmprestimo consultempres = new ConsultarEmprestimo();
-        desktoppanel.add(consultempres);
-        consultempres.setPosicao();
-        consultempres.setVisible(true);
-    }//GEN-LAST:event_subMenuConsultExempActionPerformed
-
-    private void menuSobreActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_menuSobreActionPerformed
-
-    }//GEN-LAST:event_menuSobreActionPerformed
-
-    ////Chama a tela de informações sobre o software.
-    private void subMenuSobreActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_subMenuSobreActionPerformed
-        Sobre sobre = new Sobre();
-        desktoppanel.add(sobre);
-        sobre.setPosicao();
-        sobre.setVisible(true);
-    }//GEN-LAST:event_subMenuSobreActionPerformed
-
-    //Chama a tela de consulta de devoluções.
-    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
-        ConsultarDevolucao consultdev = new ConsultarDevolucao();
-        desktoppanel.add(consultdev);
-        consultdev.setPosicao();
-        consultdev.setVisible(true);
-    }//GEN-LAST:event_jMenuItem1ActionPerformed
+    private void senhaLoginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_senhaLoginActionPerformed
+        if ((usuarioLogin.getText().equals("lyandro")) && (senhaLogin.getText().equals("1234"))) {
+            JOptionPane.showMessageDialog(null, "Acesso Garantido!");
+            TelaInicial inicial = new TelaInicial();
+            inicial.setVisible(true);
+        } else {
+            JOptionPane.showMessageDialog(null, "Acesso negado! Senha ou usuário incorreto!");
+        }
+    }//GEN-LAST:event_senhaLoginActionPerformed
 
     /**
      * @param args the command line arguments
@@ -305,21 +205,14 @@ public class Principal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JDesktopPane desktoppanel;
-    private javax.swing.JMenuBar jMenuBar1;
-    private javax.swing.JMenuItem jMenuItem1;
-    private javax.swing.JMenu menuAcervo;
-    private javax.swing.JMenu menuEmpDev;
-    private javax.swing.JMenu menuSobre;
-    private javax.swing.JMenu menuUsuarios;
-    private javax.swing.JMenuItem subMenuAdmin;
-    private javax.swing.JMenuItem subMenuCadExemplar;
-    private javax.swing.JMenuItem subMenuCadUser;
-    private javax.swing.JMenuItem subMenuConsultAcervo;
-    private javax.swing.JMenuItem subMenuConsultExemp;
-    private javax.swing.JMenuItem subMenuConsultarUser;
-    private javax.swing.JMenuItem subMenuDev;
-    private javax.swing.JMenuItem subMenuEmp;
-    private javax.swing.JMenuItem subMenuSobre;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JPasswordField senhaLogin;
+    private javax.swing.JTextField usuarioLogin;
     // End of variables declaration//GEN-END:variables
 }
